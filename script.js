619a8d36a8fc6e3dee7578bc6c8afa2186fa2c6d
sharonmathew023-blip <sharonmathew023@gmail.com>
2026-01-30 11:25:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-// Empty on purpose.
-// No fake animations.
-// No performance murder.
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
+
+toggle.onclick = () => {
+  body.classList.toggle("light");
+  body.classList.toggle("dark");
+
+  toggle.textContent = body.classList.contains("dark") ? "🌙" : "☀️";
+};
